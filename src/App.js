e32c0b14e14af986cb6e249046872a25e111ce2d
Edgar Daniel Porras Rozo <darkneit7@gmail.com>
2023-06-20 00:29:06 -0500

    feat:Se agrega listar y guardar visita

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import './styles/css/bootstrap/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/css/my-styles/style-card.css';
 import { Menu } from './component/Menu';
 import { Home } from './component/Home';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { getImagenesMascotas } from './services/imagenesServices';
 import { PropietarioVista } from './views/PropietarioVista';
-import { VisitaVista } from './views/VisitaVista';
+import { VisitData } from '././views/visitas/statefull/VisitaDataComponent';
 import { VeterinarioVista } from './views/VeterinarioVista';
 import { MascotaVista } from './views/MascotaVista';
 import { imagenesMascotasEstructura } from './data/imagenes-mascotas';
@@ -32,7 +33,7 @@ function App() {
     <Routes>
       <Route exact path='/' element={<Home dataImagenes={ dataImagenes }/>}/>
       <Route exact path='/propietario' element={<PropietarioVista/>}/>
-      <Route exact path='/visita' element={<VisitaVista/>}/>
+      <Route exact path='/visita' element={<VisitData/>}/>
       <Route exact path='/veterinario' element={<VeterinarioVista/>}/>
       <Route exact path='/mascota' element={<MascotaVista/>}/>
       <Route exact path='/' element={<Navigate to={'/'}/>}/>
---
+++ src/component/Table.jsx
@@ -0,0 +1,12 @@
+import {React} from 'react'
+import Table from 'react-bootstrap/Table';
+
+export const TableComponent = () => {
+
+    return( 
+    <div>
+        <Table responsive="sm">
+        </Table>
+    </div>
+    )
+}
